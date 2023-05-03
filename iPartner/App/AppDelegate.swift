0e4cd15fc0d2f0607b0e86f0a0da0b4d113a432c
remiie <virusloki@ya.ru>
2023-05-04 01:09:02 +0300

    add expandable search view

--- iPartner/App/AppDelegate.swift
+++ iPartner/App/AppDelegate.swift
@@ -13,7 +13,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 15, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor.white
+            ]
+            navigationBarAppearance.backgroundColor = UIColor.systemGreen
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+            
+            let tabBarApperance = UITabBarAppearance()
+            tabBarApperance.configureWithOpaqueBackground()
+            tabBarApperance.backgroundColor = UIColor.systemGreen
+            UITabBar.appearance().scrollEdgeAppearance = tabBarApperance
+            UITabBar.appearance().standardAppearance = tabBarApperance
+        }
         return true
     }
 
--- iPartner/Screens/AllCards/Controllers/AllCardsController.swift
+++ iPartner/Screens/AllCards/Controllers/AllCardsController.swift
@@ -14,11 +14,21 @@ final class AllCardsController: UIViewController, AllCardsControllerProtocol {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    private var titleView: UILabel = {
+        let label = UILabel()
+        label.text = "Список"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let searchBar = UISearchBar()
+    private let expandableView = ExpandableView()
+    private var leftConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -32,30 +42,51 @@ final class AllCardsController: UIViewController, AllCardsControllerProtocol {
     }
     
     func colorizeNavBar(_ condition: Bool) {
-        
         navigationController?.navigationBar.topItem?.title = " "
         navigationController?.navigationBar.tintColor = condition ? .white : .black
-        navigationController?.navigationBar.backgroundColor = condition ? .systemGreen : .clear
         navigationController?.navigationBar.titleTextAttributes = condition ? [
             NSAttributedString.Key.foregroundColor : UIColor.white
         ] : nil
-        if #available(iOS 15.0, *) {
-            navigationController?.navigationBar.compactScrollEdgeAppearance = navigationController?.navigationBar.compactAppearance
-        }
-        
     }
+    
+    func configureExpandableView() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        expandableView.addSubview(searchBar)
+        leftConstraint = searchBar.leftAnchor.constraint(equalTo: expandableView.leftAnchor)
+        leftConstraint.isActive = false
+        searchBar.rightAnchor.constraint(equalTo: expandableView.rightAnchor).isActive = true
+        searchBar.topAnchor.constraint(equalTo: expandableView.topAnchor).isActive = true
+        searchBar.bottomAnchor.constraint(equalTo: expandableView.bottomAnchor).isActive = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(toggleSearch))
+    }
+    
     fileprivate func configure() {
         view.backgroundColor = .systemGreen
         title = presenter?.getNavigationTitle()
         allCardsView.delegate = self
         view.addSubview(allCardsView)
-       
+        configureExpandableView()
         NSLayoutConstraint.activate([
             allCardsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             allCardsView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50),
             allCardsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             allCardsView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    @objc func toggleSearch() {
+        
+        navigationItem.titleView = expandableView
+        navigationItem.titleView?.layoutIfNeeded()
+        let isOpen = leftConstraint.isActive == true
+        leftConstraint.isActive = isOpen ? false : true
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.navigationItem.titleView?.alpha = isOpen ? 0 : 1
+            self.navigationItem.titleView?.layoutIfNeeded()
+        })
+        
+        if isOpen { navigationItem.titleView = nil }
     }
     
 }
@@ -79,8 +110,26 @@ extension AllCardsController: AllCardsViewDelegate {
     
     func showNavBar() {
         navigationController?.setNavigationBarHidden(false, animated: true)
-      
+        
     }
     
     
+}
+
+class ExpandableView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return UIView.layoutFittingExpandedSize
+    }
 }
